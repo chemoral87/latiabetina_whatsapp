6c8c50de376edf4ef8bd87ba198f5a9c8c47385d
Sergio Morales <chemoral87@hotmail.com>
2026-05-16 00:11:55 -0600

    hide params

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,16 +3,14 @@ module.exports = {
     {
       name: "WhatsappBot",
       script: "server.js",
-      cwd: "/home/pi/latiabetina_whatsapp",
+      cwd: __dirname,
       watch: false,
       instances: 1,
       autorestart: true,
       max_restarts: 5,
       env: {
-        PORT: 3007,
         NODE_ENV: "production",
-        CHROME_BIN: "/usr/bin/chromium",
       },
     },
   ],
-}
+};
